ebfe77e4da8ab53404e11418e9d28cc05ab5a1e8
Sonqo <strats_karyp@hotmail.com>
2021-07-22 08:04:47 +0300

    Simplified server file

--- server.js
+++ server.js
@@ -1,8 +1,6 @@
 const express = require('express');
-const mongoose = require('mongoose');
 
 const home = require('./routes/home');
-// const data = require('./routes/data');
 
 const dbConfig = require('./config/database-config');
 
@@ -10,18 +8,8 @@ const server = express();
 server.use(express.urlencoded({ extended: true }));
 server.use(express.json());
 
-mongoose
-    .connect(dbConfig.url, { useNewUrlParser: true })
-    .then(() => {
-        console.log('Successfully connected to database');
-    })
-    .catch((err) => {
-        console.log('Could not connect to database');
-    });
-
 const baseUrl = '/api';
 server.use(`${baseUrl}/`, home);
-// server.use(`${baseUrl}/data`, data);
 
 const port = 3000;
 server.listen(port, () => {
