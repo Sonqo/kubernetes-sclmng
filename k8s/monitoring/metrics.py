c5ccd1ed5fa0b2141bb71a0ee327b02c2dbeb363
Sonqo <strats_karyp@hotmail.com>
2021-11-19 13:56:33 +0200

    Updated structure

--- k8s/monitoring/metrics.py
+++ k8s/monitoring/metrics.py
@@ -2,14 +2,14 @@ import time, subprocess
 from kubernetes import client, config, utils
 
 config.load_incluster_config()
-
-yaml_file = '/k8s_mongo'
-
-api = client.CustomObjectsApi()
-k8s_nodes = api.list_cluster_custom_object("metrics.k8s.io", "v1beta1", "nodes")
+    
+api_metrics = client.CustomObjectsApi()
+api_stateful = client.AppsV1Api(client.ApiClient())
 
 while True:
     time.sleep(15)
+    k8s_nodes = api_metrics.list_cluster_custom_object("metrics.k8s.io", "v1beta1", "nodes")
+    replicas = api_stateful.read_namespaced_stateful_set_status('mongodb-rs', 'app').status.replicas
     for stats in k8s_nodes['items']:
         if stats['metadata']['name'] != 'cluster80':
             cpu_usage = stats['usage']['cpu']
@@ -20,5 +20,7 @@ while True:
             else:
                 raise ValueError('Unhandled CPU metric.')
             cpu_percentage = int(cpu_usage) // (2 * 10)
-            res = subprocess.run('kubectl apply -f mongo-replica_v2.yaml', shell=True, executable='/bin/bash')
-            print(res, type(res))
+            print(stats['metadata']['name'], cpu_percentage)
+            print(int(replicas))
+            # res = subprocess.run('kubectl apply -f mongo-replica_v2.yaml', shell=True, executable='/bin/bash')
+            # print(res, type(res))
