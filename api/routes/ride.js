aa545e70ee86dd212751d4b3fae1ee273e9b996e
Sonqo <strats_karyp@hotmail.com>
2021-12-10 16:28:32 +0200

    Corrections on server routes

--- api/routes/ride.js
+++ api/routes/ride.js
@@ -15,6 +15,9 @@ mongoose
 router.get('/show', async (req, res) => {
     docs = await Ride.find({
         started_at: { $gte: req.body.s_Date, $lt: req.body.e_Date },
+        start_lat: { $gte: 40, $lt: 50 },
+        start_lng: { $gte: -90, $lt: -85 },
+        start_station_id: { $gte: 200, $lt: 250 },
         member_casual: 'member',
     });
     if (!docs) {
